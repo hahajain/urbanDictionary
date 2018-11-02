2196bc4191a1aecb10f51243c8329df840842fc7
Aditya Gaydhani <adityagaydhani14@users.noreply.github.com>
2018-11-02 03:46:30 -0500

    Update storedata.py

--- storedata.py
+++ storedata.py
@@ -7,12 +7,12 @@ consumer = KafkaConsumer(
     bootstrap_servers=['ec2-52-24-5-219.us-west-2.compute.amazonaws.com:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    auto_commit_interval_ms=10,
+    auto_commit_interval_ms=0,
     group_id='my-group',
     value_deserializer=lambda x: loads(x))
 
 client = MongoClient('mongodb://ec2-52-24-5-219.us-west-2.compute.amazonaws.com:27017')
-collection = client.Urbaner.updated_dictionary
+collection = client.UrbanDictionary.updated_dictionary
 
 for message in consumer:
     message = message.value
